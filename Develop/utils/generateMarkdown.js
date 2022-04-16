514e3b16bc03815f2ff974a0c48a675fa723a7eb
Brian Samuels <samuels.brian9@gmail.com>
2022-04-15 21:10:34 -0400

    adding in additional index as well as generate markdown code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+licenseChoices = ['GPL', 'Apache', 'MIT']
+function renderLicenseBadge(license) {
+  if (license === licenseChoices[0]) {
+    return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+  } else if (license === licenseChoices[1]) {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)]'
+  } else if (license === licenseChoices[2]) {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if (license === licenseChoices[0]) {
+    return `[${licenseArr[0]}](https://www.gnu.org/licenses/agpl-3.0)`
+  } else if (license === licenseChoices[1]) {
+    return `[${licenseArr[1]}](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === licenseChoices[2]) {
+    return `[${licenseArr[22]}](https://opensource.org/licenses/MIT)`
+  } else {
+    return ''
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === licenseChoices[0]) {
+    return `Read more about ${licenseArr[0]} here:`
+  } else if (license === licenseChoices[1]) {
+    return `Read more about ${licenseArr[1]} here:`
+  } else if (license === licenseChoices[2]) {
+    return `Read more about ${licenseArr[2]} here:`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Badges
+  ${renderLicenseBadge(data.license)}
 
+  ##License 
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
 `;
 }
 
